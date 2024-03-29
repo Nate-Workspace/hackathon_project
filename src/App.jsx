48e1028cfcce483c23b53151924cefbd942b204a
yohannes mesay <johnnymesay@gmail.com>
2024-03-29 22:18:56 +0300

    add slide to Buyer

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useState } from 'react'
-import { LeftNav } from './components/LeftNav'
-import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
-
+import { useState } from "react";
+import { LeftNav } from "./components/LeftNav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import ProductsServicesEvents from "./pages/ProductsServicesEvents";
+import Seller from "./pages/Seller";
+import Buyer from "./pages/Buyer";
 function App() {
-
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
-        <LeftNav/>
+        <LeftNav />
         <Routes>
-          <Route path='/'/>
-          <Route path='/Explore'/>
-          <Route path='/Saved'/>
-          <Route path='/Cart'/>
-          <Route path='/Notification'/>
-          <Route path='/Create'/>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/product" element={<ProductsServicesEvents />} />
+          <Route path="/seller" element={<Seller />} />
+          <Route path="/buyer" element={<Buyer />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import './LeftNav.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./LeftNav.css";
+import { Link } from "react-router-dom";
 
 export const LeftNav = () => {
   return (
     <div className="navbar">
-    <div className="logo">PROJ</div>
-    <hr />
-      <div className='links'>
+      <div className="logo">PROJ</div>
+      <hr />
+      <div className="links">
         <Link to="/"> Home </Link>
-        <Link to="/Explore"> Explore </Link>
-        <Link to="/Saved"> Saved </Link>
-        <Link to="/Cart"> Cart </Link>
-        <Link to="/Notification"> Notification </Link>
-        <Link to="/Create"> Create </Link> 
+        <Link to="/dashboard"> dashboard </Link>
+        <Link to="/product"> product </Link>
+        <Link to="/seller"> seller </Link>
+        <Link to="/buyer"> buyer </Link>
+        <Link to="/product"> product </Link>
       </div>
     </div>
-  )
-}
+  );
+};
