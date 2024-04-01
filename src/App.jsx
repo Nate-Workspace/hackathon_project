50b9f720b884180e23f6553babe14d560a282e97
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 <temesgenfeni@gmail.com>
2024-04-01 23:35:38 +0300

    Add PostForm and ProfilePage files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { LeftNav } from './components/LeftNav';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Buyer from './pages/Buyer';
-
-
-function App() {
-  return (
-    <div className='App flex'>
-      <Router>
-        <div className="h-screen" style={{ width: '200px' }}>
-          <LeftNav />
-        </div>
-        <div className='flex-1'>
-          <Routes>
-            <Route path='/' element={<Dashboard />} />
-            <Route path='/Products' />
-            <Route path='/Services' />
-            <Route path='/Events' />
-            <Route path='/Buyer' element={<Buyer />}/>
-            <Route path='/Seller' />
-            <Route path='/Create' />
-          </Routes>
-        </div>
-      </Router>
-    </div>
-  );
-}
-
+import { LeftNav } from './components/LeftNav'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Dashboard from './pages/Dashboard'; 
+import Buyer from './pages/Buyer'; 
+ 
+ 
+function App() { 
+  return ( 
+    <div className='App flex'> 
+      <Router> 
+        <div className="h-screen" style={{ width: '200px' }}> 
+          <LeftNav /> 
+        </div> 
+        <div className='flex-1'> 
+          <Routes> 
+            <Route path='/' element={<Dashboard />} /> 
+            <Route path='/Products' /> 
+            <Route path='/Services' /> 
+            <Route path='/Events' /> 
+            <Route path='/Buyer' element={<Buyer />}/> 
+            <Route path='/Seller' /> 
+            <Route path='/Create' /> 
+          </Routes> 
+        </div> 
+      </Router> 
+    </div> 
+  ); 
+} 
+ 
 export default App;
